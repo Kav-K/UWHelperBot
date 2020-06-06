9bf73d9f9d8ced4d078641ec2a0680f6ff5434a0
Keith <keithchoa@gmail.com>
2020-06-05 21:50:08 -0400

    the googs

--- botCommands/bullshit.py
+++ botCommands/bullshit.py
@@ -1,29 +1,30 @@
 import discord
 from discord.ext import commands
 
+
 # bullshit
-class bullshit(commands.Cog, name = 'Bullshit'):
-	def __init__(self, bot):
-		self.bot = bot
+class bullshit(commands.Cog, name='Bullshit'):
+    def __init__(self, bot):
+        self.bot = bot
 
-		# Not really sure what this does
-		self._last_member_ = None
+        # Not really sure what this does
+        self._last_member_ = None
 
-	@commands.command()
-	async def trentslate(self, ctx, *args):
+    @commands.command()
+    async def trentslate(self, ctx, *args):
+        trentDictionary = {}
 
-		phrase = " ".join(args)
+        trentDictionary["im blue imma be im daie"] = 'https://www.youtube.com/watch?v=HgV1O0X4uXI'
+        trentDictionary[
+            "HERBO WTFWUA?"] = 'https://open.spotify.com/track/1MAF77bjR5toanBgnsMQ8k?si=kVMlN2tFQbyd3iSQKfGShw'
+        trentDictionary["the googs"] = 'http://google.com'
 
-		trentDictionary = {}
+        phrase = " ".join(args)
 
-		trentDictionary["im blue imma be im daie"] = 'https://www.youtube.com/watch?v=HgV1O0X4uXI'
-		trentDictionary["HERBO WTFWUA?"] = 'https://open.spotify.com/track/1MAF77bjR5toanBgnsMQ8k?si=kVMlN2tFQbyd3iSQKfGShw'
-		
-		try:
-			print(phrase)
-			await ctx.send(trentDictionary[phrase])
-		except:
-			await ctx.send("That isn't a supported Trent phrase")
+        if phrase == "?phrases":
+            await ctx.send(trentDictionary.keys())
 
-
-
+        try:
+            await ctx.send(trentDictionary[phrase])
+        except KeyError:
+            await ctx.send("That isn't a supported Trent phrase")
